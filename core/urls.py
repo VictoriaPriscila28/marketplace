f00051d09c9fe8ef43f0fe0bfd06e97c358c7a92
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-02-13 21:20:02 -0300

    add and rename 'marketplace folder

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,14 @@
+
 from django.urls import path
+
 from . import views
+
 
 app_name = 'core'
 
-urlpatters = [
-    path('', views.index, name = 'index'),
-    path('contact/', views.contact, name = 'contact'),
-    path('signup/', views.signup, name = 'signup')
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('signup/', views.signup, name='signup'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,23 +1,32 @@
-from django.shortcuts import render
-from item.models import Category, Item
-from .forms import SignupForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from item.models import Category, Item
+
+from .forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
 
-    return render(request, 'templates/core/index.html', {
+    return render(request, 'core/index.html', {
         'categories': categories,
         'items': items,
     })
 
 def contact(request):
-    return render(request, 'templates/core/contact.html')
+    return render(request, 'core/contact.html')
 
 def signup(request):
-    form =SignupForm()
-    return render(request, 'templates/core/signup.html')
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
 
+        if form.is_valid():
+            form.save()
 
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
