e43a83ec09b059b1402f3514993aab59f3cadbc0
VictoriaPriscila28 <victoria.priscila.pl@gmail.com>
2024-02-18 20:03:39 -0300

    edit items

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     items = Item.objects.filter(created_by=request.user)
 
-    return  render(request, 'dashboard/index.html',{
+    return render(request, 'dashboard/index.html',{
         'items': items,
     })
-
